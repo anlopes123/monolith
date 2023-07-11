8012ed37ce1b8ab6fb8f849d5002de15404397f4
Anizair Lopes <anlopes123@gmail.com>
2023-07-11 00:25:13 -0300

    Feito primeiros trabalhos no monolito

---
+++ src/modules/@shared/domain/entity/base.entity.ts
@@ -0,0 +1,28 @@
+import Id from "../value-object/id.value-object";
+
+export default class BaseEntity{
+    private _id: Id;
+    private _createdAt: Date;
+    private _updateAt: Date;
+
+    constructor(id?: Id) {
+        this._id = id;
+        this._createdAt = new Date();
+        this._updateAt = new Date();
+    }
+    get id() : Id {
+        return this._id;
+    }
+
+    get createdAt() {
+        return this._createdAt;
+    }
+
+    get updatedAt() {
+        return this._updateAt;
+    }
+
+    set updatedAt(updatedAt: Date) {
+        this.updatedAt = updatedAt;
+    }
+}
