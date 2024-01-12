d6f973640aa08f6dd25dd1a7aacd4ecfb1396661
Anizair Lopes <anlopes123@gmail.com>
2024-01-11 22:24:50 -0300

    Feito mais codificação na entidade de invoice e invoiceItems

--- src/modules/invoices/domain/InvoiceItems.ts
+++ src/modules/invoices/domain/InvoiceItems.ts
@@ -5,12 +5,36 @@ import Id from "../../@shared/domain/value-object/id.value-object"
 type InvoiceItemsPros = {
     id?: Id;
     name: string;
+    quantity: number;
     price: number;
 }
 
 export default class InvoiceItems extends BaseEntity implements AggregateRoot {
-
+    private _name: string;
+    private _price: number;
+    private _quantity: number;
     constructor(props: InvoiceItemsPros) {
-        
+        super(props.id);
+        this._name = props.name;
+        this._quantity = props.quantity;
+        this._price = props.price        
+    }
+
+    
+
+    get name(): string {
+        return this._name;        
+    }
+
+    get quantity(): number {
+        return this._quantity;
+    }
+
+    get price(): number {
+        return this._price;
+    }
+
+    total(): number {
+        return this._quantity*this._price;
     }
 }
--- src/modules/invoices/domain/invoices.ts
+++ src/modules/invoices/domain/invoices.ts
@@ -1,3 +1,4 @@
+import { ArrayDataType } from "sequelize";
 import AggregateRoot from "../../@shared/domain/entity/agregate-root.interface";
 import BaseEntity from "../../@shared/domain/entity/base.entity";
 import Address from "../../@shared/domain/value-object/address.value-object";
@@ -15,15 +16,54 @@ type InvoiceProps = {
     updatedAt?: Date;
 }
 
+
 export default class Invoices extends BaseEntity implements AggregateRoot {
     private _name: string;    
     private _document: string;
     private _address: Address;
     private _items: InvoiceItems[];
     
-        constructor(props: InvoiceProps) {
-            super(props.id);
+    constructor(props: InvoiceProps) {
+        super(props.id, props.createdAt, props.updatedAt);        
+        this._name = props.name;
+        this._document = props.document;
+        this._address = props.address;
+        this._items = props.items;
+        
+    } 
 
-        } 
+    validate(): boolean {
+        if(this._id.id.length === 0) {
+            throw new Error("Id is required");
+        }
+      
+        if(this._items.length === 0) {
+            throw new Error("Items qte must be greater than 0");
+        }
+        if(this._items.some(item=>item.quantity <=0)) {
+            throw new Error("Quantity must be greater than 0");
+        }
+        if(this.document.length === 0) {
+            throw new Error("Document is requered")
+        }
+        return true;
+    }
+
+
+    get name() : string {
+        return this._name;
+    }
+
+    get document(): string{
+        return this._document;
+    }
+
+    get address: Address {
+        return this._address;
+    }
+
+    total(): number {
+        return this._items.reduce((acc, item)=> acc+item.total(), 0);
+    }
 
 }
